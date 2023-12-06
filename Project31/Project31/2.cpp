a094762b6963865621c9a149e03458be183891a3
chuyin221111 <2974853312@qq.com>
2023-12-06 11:54:50 +0800

    排序练习

--- Project31/Project31/2.cpp
+++ Project31/Project31/2.cpp
@@ -11,14 +11,80 @@
 为每本图书的ISBN号（假设图书的ISBN号在1~1000之间）。
 
 输出格式：	
-输出也是2行，第1行为一个正整数k，表示需要买多少本书；第2行为k个用空格隔开的正整数，为从小到大已排好序的需要购买的图书ISBN号。
+输出也是2行，第1行为一个正整数k，表示需要买多少本书；第2行为k个用空格隔开的正整数，为从小到大已排好序的
+需要购买的图书ISBN号。
 */
 #include <iostream>
 using namespace std;
+const int maxsize = 100;	//排序表容量，假设为100
+typedef int datatype;	//假设关键字为int
+typedef int othertype;
+typedef struct {
+	datatype key;		//关键字域
+	//othertype other;		//其它域
+} rectype;				//记录类型
+typedef rectype list[maxsize + 1];
+//排序表类型，0号单元不用（或作它用，如“监视哨”）
+int Partition(list R, int p, int q) {
+	//对无序区R[p]到R[q]划分，返回划分后基准的位置
+	int i, j;
+	i = p;  j = q;   R[0] = R[i];
+	//R[0]作辅助量，存基准(无序区第一个记录)
+	while (i < j) {
+		while (R[j].key >= R[0].key && i < j) j--;
+		//从右向左扫描
+		if (i < j) { R[i] = R[j]; i++; }	//交换R[i]和R[j]
+		while (R[i].key <=R[0].key && i < j) i++;
+		//从左向右扫描
+		if (i < j) { R[j] = R[i]; j--; }	//交换R[i]和R[j]
+	}
+	R[i] = R[0];			//将基准移到最后位置
+	return i;
+}
+void QuickSort(list R, int s, int t) {
+	//对R[s]到R[t]快速排序
+	int i;
+	if (s >= t) return;	//只有一个记录或无记录无需排序
+	i = Partition(R, s, t);	//对R[s]到R[t]做划分
+	QuickSort(R, s, i-1);	//递归处理左区间
+	QuickSort(R, i + 1, t);	//递归处理右区间
+}
+void print(list R, int n)
+{
+	int g = n;
+	for (int y = 1; y <= n; y++)
+	{
+		if (R[y].key == R[y + 1].key)
+		{
+			g--;
+		}
+	}
+	cout << g<<endl;
+	for (int h = 1; h <= n; h++)
+	{
+		if (R[h].key == R[h + 1].key)
+		{
+			h++;
+			cout << R[h].key<<" ";
+		}
+		else
+		{
+			cout << R[h].key<<" ";
+		}
+	}
+
+}
 int main()
 {
 	int n;
 	cin >> n;
-	int *arr= new int[n];
-
+	list R;
+	int k;
+	for ( k = 1; k <= n; k++)
+	{
+		cin >>R[k].key;
+	}
+	QuickSort(R, 1, n);
+	print(R, n);
+	
 }
